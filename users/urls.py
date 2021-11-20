de71a6ff3fdf6b81a12c421409a33cefe59aa57b
nicollevherrerap <nvherrerapi@unal.edu.co>
2021-11-20 12:44:00 -0500

    first ms done

---
+++ users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import UserListCreate,UserUpdateDelete, RoleListCreate, RoleUpdateDelete 
+
+urlpatterns = [
+    path('rol/' , RoleListCreate.as_view()),
+    path('rol/<pk>/', RoleUpdateDelete.as_view()),
+    path('usuario/', UserListCreate.as_view()),
+    path('usuario/<pk>', UserUpdateDelete.as_view())
+]
