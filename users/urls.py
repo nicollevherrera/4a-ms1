e5f70d1ce0b5123f1b8b61c2e407a4f1cb6e9f02
nicollevherrerap <nvherrerapi@unal.edu.co>
2021-11-21 10:58:15 -0500

    error urls usuario

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('rol/' , RoleListCreate.as_view()),
     path('rol/<pk>/', RoleUpdateDelete.as_view()),
     path('usuario/', UserListCreate.as_view()),
-    path('usuario/<pk>', UserUpdateDelete.as_view())
+    path('usuario/<pk>/', UserUpdateDelete.as_view())
 ]
